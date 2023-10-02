2270a3144595e273cae6593fca575a97e55477e0
rjwyse <wyse.rob27@gmail.com>
2023-10-02 14:55:23 -0400

    Lab P5

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
@@ -15,11 +19,14 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', { 'dog': dog })
+  id_list = dog.toys.all().values_list('id')
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form, 'toys': toys_dog_doesnt_have })
 
 class DogCreate(CreateView):
   model = Dog
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
 
 class DogUpdate(UpdateView):
   model = Dog
@@ -28,3 +35,38 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
   model = Dog
   success_url = '/dogs'
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys' 
+
+def assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+def unassoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
